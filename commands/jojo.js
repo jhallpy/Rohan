f9e37b7e19da3e7965c0b99b1d0e270a552002cd
Joshua Hall <alamoscout117@gmail.com>
2019-08-06 06:54:17 -0400

    Removed no longer in use code

--- commands/jojo.js
+++ commands/jojo.js
@@ -1,5 +1,5 @@
 let { jojos } = require('../assets/arrays/jojos.json');
-const misc = require('../utils/misc.js');
+let misc = require('../utils/misc.js');
 
 exports.run = (client, message, args) => {
     message.channel.send(misc.randomInArray(jojos));
--- commands/santa.js
+++ commands/santa.js
@@ -1,10 +1,8 @@
-let help = require('../assets/arrays/text.json')
+/*let help = require('../assets/arrays/text.json')
 exports.run = (client, message, args) => {
     if(args === undefined || args.length === 0) message.channel.send(help.santa);
     else{
-        
         const command = args[0].toLowerCase().replace(/[\W_]+/g,"");
-
         try{
             let commandFile = require(`../commands/santa/${command}.js`);
             commandFile.run(client, message, args);
@@ -14,4 +12,7 @@ exports.run = (client, message, args) => {
             message.channel.send('Sorry, I don\'t recognize that command.');
         }
     }
+}*/
+exports.run = (client, message, args) => {
+    message.channel.send('Currently under construction.');
 }
