d36a103e1b0dae326e98abeaa435c8494673e24e
Joshua Hall <alamoscout117@gmail.com>
2019-08-08 15:28:33 -0400

    Added new command and added more readability

--- rohan.js
+++ rohan.js
@@ -3,12 +3,11 @@ Hello and welcome to Rohan!
 */
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const mysql = require('mysql');
 const fs = require('fs');
 
-const misc = require("./utils/misc");
 const config = require ("./config.json");
 client.config = config;
+const sing = require('./assets/arrays/sing.json');
 
 const specialComFolder = './special_commands/';
 const commandFolder = './commands/';
@@ -17,11 +16,9 @@ var specialCommands = [];
 var specialRegex = '';
 var commands = [];
 var regex = '';
+var singingRegex = '';
 
-
-client.on('ready', () => {
-  console.log(`I, ${client.user.username}, am ready.`);
-});
+client.on('ready', () => {console.log(`I, ${client.user.username}, am ready.`);});
 
 var promise = new Promise((resolve, reject) => {
   fs.readdir(commandFolder, (err,files) => {
@@ -57,24 +54,36 @@ var promise1 = new Promise((resolve, reject) => {
 .catch((err) =>{
   console.log(err);
 });
+for(var i = 0; i < sing.lyrics.length; i++){
+  singingRegex += "\|\\b"+sing.lyrics[i].input+"\\b";
+}
+singingRegex = singingRegex.slice(1);
+singingRegex = new RegExp(singingRegex, 'g');
 //TODO: Convert to async and make event handlers placed in a separate folder.
 //TODO: Add in escaping for the user input. Currently works well. 
 //TODO: Add new prompt command with array.
+//TODO: Increment version numbers...
 
 client.on('message', message => {
   let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   let command = args.shift().toLowerCase().replace(/[\W_]+/g,"");
+ try{
   //Ignores all bots, MUST BE AT THE TOP. Avoids infinite loops.
   if (message.author.bot) return;
 
   //must check for prefix, will mess up other commands if it doesn't.
+  
   else if(message.content.indexOf(config.prefix) !== 0){
     if (message.content.match(specialRegex) !== null){
       let commandFile = require(`./special_commands/${message.content.match(specialRegex)[0]}.js`);
-      commandFile.run(client, message);
+      commandFile.run(client, message, args);
+    }
+    else if (message.content.match(singingRegex) !== null){
+      args = message.content.match(singingRegex)[0];
+      let commandFile = require(`./special_commands/sing.js`);
+      commandFile.run(client, message, args);
     }
   }
-
   //Ignores messages that don't start with prefix. Also checks against strikethroughs.
   else if (message.content.indexOf(config.prefix) !== 0 || message.content.lastIndexOf(config.prefix) > 0) return;
 
@@ -84,5 +93,10 @@ client.on('message', message => {
   }
   else
     message.channel.send('Sorry, I don\'t recognize that command.');
+
+}
+catch(err){
+  console.log(err)
+}
 });
 client.login(config.token);
---
+++ special_commands/sing.js
@@ -0,0 +1,6 @@
+let sing = require("../assets/arrays/sing.json")
+exports.run = (client, message, args) => {
+    sing.lyrics.forEach(x =>{
+        if(args === x.input) message.channel.send(x.reply);
+    })
+}
