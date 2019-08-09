753080829b8ed18a3b9001382eb01aad69b48b6f
Joshua Hall <alamoscout117@gmail.com>
2019-08-09 08:15:54 -0400

    New command + dev dependency update

---
+++ commands/prompt.js
@@ -0,0 +1,17 @@
+let prompt = require('../assets/arrays/prompt.json');
+let misc = require('../utils/misc.js');
+exports.run = (client, message, args) =>{
+    console.log(args);
+    if(args === undefined || args.length == 0){
+        message.channel.send(misc.randomInArray(prompt.prompt));
+    }
+    else if(args.length == 1){
+        message.channel.send(misc.randomInArray(prompt.onePrompt)
+        .replace(/\$person/g, args[0]));
+    }
+    else if(args.length == 2){
+        message.channel.send(misc.randomInArray(prompt.twoPrompt)
+        .replace(/\$person1/g, args[0])
+        .replace(/\$person2/g, args[1]));
+    }
+}
