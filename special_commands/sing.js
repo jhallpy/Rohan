b989fd2994c9c275d7c1a767386a98c3a8077ae8
Joshua Hall <alamoscout117@gmail.com>
2019-08-10 08:24:06 -0400

    fixed bug

--- special_commands/sing.js
+++ special_commands/sing.js
@@ -1,4 +1,4 @@
-let { sing } = require("../assets/arrays/sing.json")
+const sing = require("../assets/arrays/sing.json")
 exports.run = (client, message, args) => {
     sing.lyrics.forEach(x =>{
         if(args === x.input) message.channel.send(x.reply);
