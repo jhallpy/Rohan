8fc4b9074b91d9922ef7b276772875363d2c2340
Joshua Hall <alamoscout117@gmail.com>
2019-11-10 22:10:30 -0500

    version number change

--- commands/prompt.js
+++ commands/prompt.js
@@ -1,11 +1,13 @@
 'use strict';
-const prompt = require('../assets/arrays/prompt.json');
+//const prompt = require('../assets/arrays/prompt.json');
 const misc = require('../utils/misc.js');
 module.exports = {
   name: 'prompt',
   usage: '~prompt',
-  description: 'If you have ideas for prompts, please message Aver. Currently under construction.',
+  description: 'Currently being worked on. We could use ideas for prompts. The intended purpose is to have writing prompts containing up to currently, 2 people.',
   execute(client, message, args){
+    message.channel.send('Currently being worked on. We could use ideas for prompts. The intended purpose is to have writing prompts containing up to currently, 2 people.');
+    /*
     if (args === undefined || args.length === 0){
       message.channel.send(misc.randomInArray(prompt.prompt));
     } else if (args.length === 1){
@@ -15,6 +17,6 @@ module.exports = {
       message.channel.send(misc.randomInArray(prompt.twoPrompt)
         .replace(/\$person1/g, args[0])
         .replace(/\$person2/g, args[1]));
-    }
+    }*/
   },
 };
